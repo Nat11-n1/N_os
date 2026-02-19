3987df92e0f84351ea26a741fc7907de05f6ffa1
Nat11-n1 <nat11n1n1@gmail.com>
2026-02-19 15:38:05 +0100

    feat: add terminal command manager for kernel
    Implement basic command parsing logic in `terminal_commande_manageur.c`. Add support for the `clear` command to reset the terminal display. Include string parsing to separate main commands from arguments/objects. Add corresponding header file for kernel integration.

---
+++ kernel/drivers/terminal/terminal_commande_manageur.c
@@ -0,0 +1,49 @@
+#include "terminal_commande_manageur.h"
+#include "kernel/lib/strcmp.h"
+#include "kernel/drivers/terminal/terminal.h"
+
+#define maxmaincommande 25
+#define maxobjectcommande 100
+
+static unsigned char maincommande[maxmaincommande];
+static unsigned char objectcommande[maxobjectcommande];
+
+void execute_commande(unsigned char *commande){
+    int i = 0;
+    int j = 0;
+    maincommande[0] = '\0';
+    objectcommande[0] = '\0';
+
+    while (commande[i]==' '){
+        i++;
+    }
+
+    for (j = 0; commande[i] != '\0' && j < maxmaincommande-1;i++){
+        if (commande[i]!=' '){
+            maincommande[j] = commande[i];
+            j++;
+        }
+        else{
+            while (commande[i]==' '){
+                i++;
+            }
+            maincommande[j]='\0';
+            break;
+        }
+    }
+    for (j = 0; commande[i] != '\0'&& j < maxobjectcommande-1;j++,i++){
+            objectcommande[j] = commande[i];
+    }
+    objectcommande[j] = '\0';
+    find_commande(maincommande);
+}
+
+void find_commande(unsigned char* commande){
+    if (issamestr(commande,"clear")){
+        clear_terminal();
+    }
+    else{
+        print_str(commande);
+        print_str(" : unknown command !\n");
+    }
+}
---
+++ kernel/drivers/terminal/terminal_commande_manageur.h
@@ -0,0 +1,7 @@
+#ifndef terminal_commande_manageur_h
+#define terminal_commande_manageur_h
+
+void execute_commande(unsigned char *commande);
+void find_commande(unsigned char* commande);
+
+#endif
