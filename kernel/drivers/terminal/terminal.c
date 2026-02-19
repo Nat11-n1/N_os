67ca229f26b569e9ff54a76f7706647afd6c53ca
Nat11-n1 <nat11n1n1@gmail.com>
2026-02-19 13:28:25 +0100

    Remove static from VGA_screen
    Make VGA_screen a non-static symbol so it can be referenced from other translation units. Previously the pointer was file-local (static), limiting external access; no other logic was changed.

--- kernel/drivers/terminal/terminal.c
+++ kernel/drivers/terminal/terminal.c
@@ -4,7 +4,7 @@ static int currentscreensizex = 0;
 static int currentscreensizey = 0;
 static int maxscreensizex = 80;
 static int maxscreensizey = 25;
-static unsigned char* VGA_screen = (unsigned char*) 0xb8000;
+unsigned char* VGA_screen = (unsigned char*) 0xb8000;
 
 void print_char(char character){
     if (character=='\n'){
