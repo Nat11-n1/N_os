0d520612bc32dff54172bdaacaa20863e3bdbc55
Nat11-n1 <nat11n1n1@gmail.com>
2026-02-20 21:32:10 +0100

    feat : start inplementing a hexadecimale utilitise

---
+++ kernel/lib/hex_utils.c
@@ -0,0 +1,9 @@
+char to_hex(char*value){
+    char hexa = "";
+    return hexa;
+}
+
+char to_val(char*hexa){
+    char value = "";
+    return value;
+}
---
+++ kernel/lib/hex_utils.h
@@ -0,0 +1,7 @@
+#ifndef hex_utils_h
+#define hex_utils_h
+
+char to_hex(char*value);
+char to_val(char*hexa);
+
+#endif
