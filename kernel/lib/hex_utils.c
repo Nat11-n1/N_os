a39c70517f4492c9ccbbebe948613f4050b4de01
Nat11-n1 <nat11n1n1@gmail.com>
2026-02-22 15:49:17 +0100

    update hex_utils
    add diferent value

--- kernel/lib/hex_utils.c
+++ kernel/lib/hex_utils.c
@@ -1,9 +1,18 @@
-char to_hex(char*value){
+char int_to_hex(int value){
     char hexa = "";
     return hexa;
 }
 
-char to_val(char*hexa){
+char hex_to_int(char*hexa){
+    char value = "";
+    return value;
+}
+char char_to_hex(char*value){
+    char hexa = "";
+    return hexa;
+}
+
+char hex_to_char(char*hexa){
     char value = "";
     return value;
 }
--- kernel/lib/hex_utils.h
+++ kernel/lib/hex_utils.h
@@ -1,7 +1,9 @@
 #ifndef hex_utils_h
 #define hex_utils_h
 
-char to_hex(char*value);
-char to_val(char*hexa);
+char int_to_hex(int value);
+char hex_to_int(char*hexa);
+char char_to_hex(char*value);
+char hex_to_char(char*hexa);
 
 #endif
