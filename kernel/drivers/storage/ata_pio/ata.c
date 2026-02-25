eb7eaeae4c377f6aa3dfa7fd60091f5bf5012b82
Nat11-n1 <nat11n1n1@gmail.com>
2026-02-25 21:27:28 +0100

    feate : finish the ata.c files
    implement 4 fonction in ata.c:
    - ata_init()
    -ata_wait()
    -ata_read_sector()
    ata_write_sector()
    for after being used in the fat32 files and and call ata_init() in kernel.c

--- kernel/drivers/storage/ata_pio/ata.c
+++ kernel/drivers/storage/ata_pio/ata.c
@@ -1,15 +1,52 @@
 #include "ata.h"
 
+int diskready = 0;
+
 void ata_init() {
-    uint8_t inb(uint16_t port);
-    void outb(uint16_t port, uint8_t value);
+    outb(0x1F6, 0xA0);
+    outb(0x1F7, 0xEC);
+    if (inb(0x1F7) == 0){
+        return; // No drive found
+    }else{
+        ata_wait(); // Drive found
+    }
 }
 
 void ata_wait() {
+    while (diskready==0){
+        uint8_t status = inb(0x1F7);
+        if ((status & 0x80) == 0 && (status & 0x40) != 0){
+            diskready = 1; //disk is ready
+        }
+    }
 }
 
 void ata_read_sector(uint32_t lba, uint8_t* buffer) {
+    if (diskready==1){
+        outb(0x1F2, 1);
+        outb(0x1F3, lba & 0xFF);
+        outb(0x1F4, (lba >> 8) & 0xFF);
+        outb(0x1F5, (lba >>16)& 0xFF);
+        outb(0x1F6, 0xE0 | ((lba>>24) & 0x0F));
+        outb(0x1F7,0x20);
+        ata_wait();
+        for (int i =0;i<256;i++){
+            ((uint16_t*)buffer)[i] = inw(0x1F0);
+        }
+    }
 }
 
 void ata_write_sector(uint32_t lba, uint8_t* buffer) {
+    if (diskready==1){
+        outb(0x1F2, 1);
+        outb(0x1F3, lba & 0xFF);
+        outb(0x1F4, (lba >> 8) & 0xFF);
+        outb(0x1F5, (lba >>16)& 0xFF);
+        outb(0x1F6, 0xE0 | ((lba>>24) & 0x0F));
+        outb(0x1F7,0x30);
+        ata_wait();
+        for (int i =0;i<256;i++){
+            outw(0x1F0, ((uint16_t*)buffer)[i]);
+        }
+    }
 }
--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,6 +1,8 @@
-#include "drivers/terminal/terminal.h"
+#include "drivers\terminal\terminal.h"
+#include "drivers\storage\ata_pio\ata.h"
 
 void _start(void){
+    ata_init();
     start_terminal();
     while(1);
 }
