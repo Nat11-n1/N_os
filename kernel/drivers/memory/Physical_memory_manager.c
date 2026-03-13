8350f8b2d1952bf521ce3fb7ead63374a1f929ca
Nat11-n1 <nat11n1n1@gmail.com>
2026-03-13 19:14:35 +0100

    feat : finish the init fonction in the PMM files
    add a protection to the ram of the bios, kernel, and the bitmap

--- kernel/drivers/memory/Physical_memory_manager.c
+++ kernel/drivers/memory/Physical_memory_manager.c
@@ -20,13 +20,27 @@ void PMM_init(void){
     }
     uint8_t *bitmap = (uint8_t *)&_kernel_end;
     uint64_t total_pages = RAM_Total/4096;
-    uint64_t bitmap_size = (RAM_Total+7)/8;
-    for (int i =0; i=bitmap_size-1;i++){
-        map[i].size = 0xFF;
+    uint64_t bitmap_size = (total_pages + 7)/8;
+    for (int i =0; i<bitmap_size;i++){
+        bitmap[i] = 0xFF;
     }
-     for (int i =0;i!=*entry_count ;i++){
-        if (map[i].type = 1){
-            
+    for (int i =0;i!=*entry_count ;i++){
+        if (map[i].type == 1){
+            uint64_t first_page = map[i].base/4096;
+            uint64_t nbr_pages = map[i].size/4096;
+            for (uint64_t current_page = first_page;current_page<nbr_pages+first_page;current_page++){
+                bitmap[current_page / 8] &= ~(1 << (current_page % 8));
+            }
         }
+    }
+    //protect the bios and kernel in the ram
+    for (uint64_t current = 0x0;current<0x100000/4096;current++){
+        bitmap[current / 8] |= (1 << (current % 8));
+    }
+    for (uint64_t current = 0x10000/4096;current < (uint64_t)&_kernel_end/4096;current++){
+        bitmap[current / 8] |= (1 << (current % 8));
+    }
+    for (uint64_t current = (uint64_t)&_kernel_end/4096;current<(uint64_t)&_kernel_end/4096+(bitmap_size+4095)/4096;current++){
+        bitmap[current / 8] |= (1 << (current % 8));
     }
 }
