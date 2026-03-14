6639454959e752ecf1e3f5f7e9910fce7d0c7768
Nat11-n1 <nat11n1n1@gmail.com>
2026-03-14 15:43:43 +0100

    feat : add PMM_alloc_page and PMM_free_page to physical_memory_manageur.c
    add to fonction :
    -PMM_alloc_page for allocat page from the ram to app
    -PMM_free_page for free page in the ram

--- kernel/drivers/memory/Physical_memory_manager.c
+++ kernel/drivers/memory/Physical_memory_manager.c
@@ -3,6 +3,9 @@
 
 extern uint64_t _kernel_end;
 uint64_t RAM_Total = 0;
+uint8_t *bitmap = (uint8_t *)&_kernel_end;
+uint64_t total_pages;
+uint64_t bitmap_size;
 struct entryE820
 {
     uint64_t base;
@@ -18,9 +21,8 @@ void PMM_init(void){
             RAM_Total = map[i].base  +map[i].size;
         }
     }
-    uint8_t *bitmap = (uint8_t *)&_kernel_end;
-    uint64_t total_pages = RAM_Total/4096;
-    uint64_t bitmap_size = (total_pages + 7)/8;
+    total_pages = RAM_Total/4096;
+    bitmap_size = (total_pages + 7)/8;
     for (int i =0; i<bitmap_size;i++){
         bitmap[i] = 0xFF;
     }
@@ -42,5 +44,26 @@ void PMM_init(void){
     }
     for (uint64_t current = (uint64_t)&_kernel_end/4096;current<(uint64_t)&_kernel_end/4096+(bitmap_size+4095)/4096;current++){
         bitmap[current / 8] |= (1 << (current % 8));
+    }
+}
+
+uint64_t PMM_alloc_page(void){
+    for (uint64_t page = 0;page <total_pages;page++){
+        if (!(bitmap[page/8] & (1 << (page%8)))){
+            bitmap[page/8] |= (1 << (page%8));
+            return page*4096;
+        }
+    }
+    return 0xFFFFFFFFFFFFFFFF;
+}
+
+int PMM_free_page(uint64_t value){
+    if (value % 4096 == 0){
+        uint64_t page = value / 4096;
+        bitmap[page/8] &= ~(1 << (page%8));
+        return 1;
+    }
+    else{
+    return 0;
     }
 }
