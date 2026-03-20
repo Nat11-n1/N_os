5f7b6829d544fcce26aae8dbae410bbd91e0880c
Nat11-n1 <nat11n1n1@gmail.com>
2026-03-20 21:13:17 +0100

    Update utils.h
    add a invlpg for Invalidate TLB Entry

--- kernel/lib/utils.h
+++ kernel/lib/utils.h
@@ -1,9 +1,15 @@
 #ifndef utils_h
 #define utils_h
 
+#include "types.h"
+
 static inline void halt(void){
     __asm__ volatile("cli");
     __asm__ volatile("hlt");
+}
+
+static inline void invlpg(uint64_t addr){
+    __asm__ volatile("invlpg (%0)" :: "r"(addr) : "memory");
 }
 
 #endif
